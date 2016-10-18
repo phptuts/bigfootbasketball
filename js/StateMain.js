44a410bbb963b91395657c793765edbe51ae8f62
phptuts <php.noah@gmail.com>
2016-10-17 23:34:51 -0500

    Got Cursor / sprite animations with json working

--- js/StateMain.js
+++ js/StateMain.js
@@ -1,11 +1,11 @@
-var bigFootSprite;
+var bigFootSprite, cursors;
 
 var StateMain={    
     
    preload:function()
     {
         game.load.image('background','images/forest.png');
-        game.load.atlasXML('bigfoot', 'images/Yeti.png', 'xml/yeti.xml');   
+        game.load.atlas('bigfoot', 'images/bigfoot.png', 'json/bigfoot.json');   
      },
     
     create:function()
@@ -16,14 +16,27 @@ var StateMain={
 
         bigFootSprite = game.add.sprite(100, 100, 'bigfoot');
         bigFootSprite.scale.setTo(2, 2);
-        var walk = bigFootSprite.animations.add('walk');
-        bigFootSprite.animations.play('walk', 6, true);
+        bigFootSprite.animations.add('walk', Phaser.Animation.generateFrameNames('bigfoot', 21, 29), 5, true);
+        bigFootSprite.animations.play('walk');
+    
+        cursors = game.input.keyboard.createCursorKeys();
 
     },
     
     update:function()
     {       
-        
+        this.controls();
+    },
+
+    controls: function() 
+    {
+        if (cursors.right.isDown) {
+
+        }
+
+        if (cursors.left.isDown) {
+            
+        }
     }    
     
 }
