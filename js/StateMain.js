f1a3b4a70a0a78651884893f1c10681f955b0faa
phptuts <php.noah@gmail.com>
2016-10-20 00:07:38 -0500

    Got Basketball to shoot up

--- js/StateMain.js
+++ js/StateMain.js
@@ -1,26 +1,44 @@
-var bigFootSprite, cursors;
+var bigFootSprite, cursors, direction, hoop, spaceKey, shootingBall;
 
 var StateMain={    
     
    preload:function()
     {
         game.load.image('background','images/forest.png');
+        game.load.image('basketball', 'images/basketball.png');
         game.load.atlas('bigfoot', 'images/bigfoot.png', 'json/bigfoot.json');   
+        game.load.image('hoop', 'images/hoop.png');
      },
     
     create:function()
     {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         backgroundImage = game.add.image(-30,0,'background');
         backgroundImage.width = game.width + 30;
         backgroundImage.height = game.height;
 
-        bigFootSprite = game.add.sprite(100, 100, 'bigfoot');
+
+
+        bigFootSprite = game.add.sprite(300, 500, 'bigfoot');
         bigFootSprite.scale.setTo(2, 2);
-        bigFootSprite.animations.add('walk', Phaser.Animation.generateFrameNames('bigfoot', 21, 29), 5, true);
-        bigFootSprite.animations.play('walk');
-    
+        bigFootSprite.anchor.setTo(.5, .5);
+        bigFootSprite.animations.add('walk', Phaser.Animation.generateFrameNames('bigfoot', 21, 26), 5, true);
+
+        basketball = game.add.sprite(100, 100, 'basketball');
+        basketball.scale.setTo(.2, .2);
         cursors = game.input.keyboard.createCursorKeys();
 
+        game.physics.enable([bigFootSprite, basketball], Phaser.Physics.ARCADE);
+        bigFootSprite.body.collideWorldBounds=true;
+        basketball.body.collideWorldBounds = true;
+        bigFootSprite.body.gravity.y = 1000;
+
+        game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
+	    spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        
+        hoop = game.add.sprite(50, 120, 'hoop');
+        hoop.scale.setTo(.2, .2);
+        hoop.anchor.setTo(.5, .5);
     },
     
     update:function()
@@ -30,12 +48,47 @@ var StateMain={
 
     controls: function() 
     {
-        if (cursors.right.isDown) {
+        console.log(basketball.y);
+        if (basketball.y > 420) {
+            shootingBall = false;
+        }
 
+        if (cursors.right.isDown) {
+            bigFootSprite.scale.setTo(2, 2);
+            bigFootSprite.body.velocity.x = 200;
+            bigFootSprite.animations.play('walk');
+            direction = 'right';
         }
 
         if (cursors.left.isDown) {
-            
+            bigFootSprite.scale.setTo(-2, 2);
+            bigFootSprite.body.velocity.x = -200;
+            bigFootSprite.animations.play('walk');
+            direction = 'left';
+        }
+
+        if (cursors.down.isDown) {
+            bigFootSprite.body.velocity.x = 0;
+            bigFootSprite.animations.stop();
+        }
+
+        if (cursors.up.isDown) {
+            bigFootSprite.body.velocity.y = -500;
+            bigFootSprite.animations.stop();
+        }
+
+        if (spaceKey.downDuration(4000)) {
+            shootingBall = true;
+            basketball.body.velocity.x = -300;
+            basketball.body.gravity.y = 400;
+            basketball.body.velocity.y = -600;
+        }
+
+        if (!shootingBall) {
+            var ballXPixels = (direction === 'right') ? 20 : -60;
+
+            basketball.x = bigFootSprite.x + ballXPixels;
+            basketball.y = bigFootSprite.y - 20;
         }
     }    
     
