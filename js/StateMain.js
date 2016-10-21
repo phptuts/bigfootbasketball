bfba82bb86eba7b05943b814a805767fc8dc8e7c
phptuts <php.noah@gmail.com>
2016-10-20 23:22:31 -0500

    Got more physics with the basketball to work

--- js/StateMain.js
+++ js/StateMain.js
@@ -8,6 +8,7 @@ var StateMain={
         game.load.image('basketball', 'images/basketball.png');
         game.load.atlas('bigfoot', 'images/bigfoot.png', 'json/bigfoot.json');   
         game.load.image('hoop', 'images/hoop.png');
+        game.load.image('back-hoop', 'images/backhoop.png');
      },
     
     create:function()
@@ -28,27 +29,43 @@ var StateMain={
         basketball.scale.setTo(.2, .2);
         cursors = game.input.keyboard.createCursorKeys();
 
-        game.physics.enable([bigFootSprite, basketball], Phaser.Physics.ARCADE);
-        bigFootSprite.body.collideWorldBounds=true;
-        basketball.body.collideWorldBounds = true;
-        bigFootSprite.body.gravity.y = 1000;
+
 
         game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
 	    spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         
-        hoop = game.add.sprite(50, 120, 'hoop');
+        hoop = game.add.sprite(60, 180, 'hoop');
         hoop.scale.setTo(.2, .2);
         hoop.anchor.setTo(.5, .5);
+
+        backhoop = game.add.sprite(0, 70, 'back-hoop');
+
+        game.physics.enable([bigFootSprite, basketball, hoop, backhoop], Phaser.Physics.ARCADE);
+        bigFootSprite.body.collideWorldBounds=true;
+        basketball.body.collideWorldBounds = true;
+        basketball.outOfBoundsKill = true;
+        bigFootSprite.body.gravity.y = 1000;
+    
+        basketball.body.onCollide = new Phaser.Signal();
+        basketball.body.onCollide.add(this.hitBasketBall, this);
+        hoop.body.immovable = true;
+
     },
     
     update:function()
-    {       
+    {     
+            game.physics.arcade.collide(basketball, hoop);
+  
         this.controls();
     },
 
+    hitBasketBall: function(basketball, hoop) {
+        console.log(basketball);
+        console.log(hoop);
+    }, 
+
     controls: function() 
     {
-        console.log(basketball.y);
         if (basketball.y > 420) {
             shootingBall = false;
         }
@@ -77,16 +94,20 @@ var StateMain={
             bigFootSprite.animations.stop();
         }
 
-        if (spaceKey.downDuration(4000)) {
+        if (spaceKey.downDuration(4000) && !shootingBall) {
             shootingBall = true;
             basketball.body.velocity.x = -300;
+            basketball.body.bounce.setTo(.4);
             basketball.body.gravity.y = 400;
-            basketball.body.velocity.y = -600;
+            basketball.body.velocity.y = -500;
         }
 
         if (!shootingBall) {
             var ballXPixels = (direction === 'right') ? 20 : -60;
-
+            basketball.body.velocity.x = 0;
+            basketball.body.bounce.setTo(0);
+            basketball.body.gravity.y = 0;
+            basketball.body.velocity.y = 0;
             basketball.x = bigFootSprite.x + ballXPixels;
             basketball.y = bigFootSprite.y - 20;
         }
